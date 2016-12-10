234a807e6b3e86b7528865cb489f6cb4f74dc82a
Mirbek Askerov <mirbek.askerov@gmail.com>
2016-12-10 17:31:17 +0600

    Change indentation from route.rb via GitHub

--- module3/route.rb
+++ module3/route.rb
@@ -16,21 +16,21 @@ class Route
   end
 
   def add_station(transitional_point)
-  	@stations.insert(-2, transitional_point)
-  	puts "New station #{transitional_point.station_name} was added!"
+    @stations.insert(-2, transitional_point)
+    puts "New station #{transitional_point.station_name} was added!"
   end
 
   def destroy_station(deleting_station)
-  	if @stations.include? deleting_station
-  	  puts "The station #{deleting_station.station_name} is deleting from route."
-  	  @stations.delete(deleting_station)
-  	else
-  	  puts "The station #{deleting_station.station_name} is not found!"
-  	end
+    if @stations.include? deleting_station
+      puts "The station #{deleting_station.station_name} is deleting from route."
+      @stations.delete(deleting_station)
+    else
+      puts "The station #{deleting_station.station_name} is not found!"
+    end
   end
 
   def show_all_route_stations
-  	puts "All stations on the routes: "
-  	@stations.each { |station| puts station.station_name }
+    puts "All stations on the routes: "
+    @stations.each { |station| puts station.station_name }
   end
 end
